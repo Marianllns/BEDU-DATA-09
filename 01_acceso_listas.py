85f5ad33a9f712435581eac8a96a18849ca15b1d
Marianllns <mariana.alonllns@outlook.com>
2024-01-29 19:42:02 -0600

    car crashes dataset

---
+++ 01_acceso_listas.py
@@ -0,0 +1,16 @@
+frutas = list()
+
+print(frutas)
+
+frutas.append('manzana')
+frutas.append('plátano')
+frutas.append('sandía')
+frutas.append('fresas')
+frutas.append('coco')
+print(frutas)
+
+#acceder al penúltimo
+
+print(frutas[-2])
+
+print(len(frutas))
